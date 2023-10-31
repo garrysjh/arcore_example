0774bf81f2b12d42df27d922415bb1268abbe3b7
Garry <garrysjh@gmail.com>
2023-10-31 19:45:25 +0800

    Fixed for new android

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:arcore_example/views/homeView.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,15 +16,12 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          textTheme:
-              GoogleFonts.nunitoSansTextTheme(Theme.of(context).textTheme),
           appBarTheme: AppBarTheme(
               backgroundColor: Colors.transparent,
               elevation: 0,
               centerTitle: true,
               iconTheme: const IconThemeData(color: Colors.black),
-              titleTextStyle:
-                  GoogleFonts.nunito(color: Colors.black, fontSize: 20))),
+)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
